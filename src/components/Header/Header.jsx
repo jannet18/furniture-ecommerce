79faa6470d38ea0f05716d066a96728b9e483f98
janet <janetngei18@gmail.com>
2023-03-25 16:48:03 +0300

    home component and styling complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import './Header.css';
-import { motion } from 'framer-motion';
-import logo from '/home/janet/development/code/react-firebase/furniture-ecommerce/src/assets/logo1.jpg';
+import React from "react";
+import "./Header.css";
+import { motion } from "framer-motion";
+import logo from "../../assets/images/logo1.jpg";
 import { Container, Row } from "reactstrap";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
   return (
-    <header className='header'>
+    <header className="header">
       <Container>
         <Row>
           <div className="nav__wrapper">
             <div className="logo">
-            <img src={logo} alt="logo" />
+              <img src={logo} alt="logo" />
+
               <div>
                 <h1>FCI FURNITURE</h1>
                 <p>Since 1998</p>
@@ -23,31 +22,44 @@ const Header = () => {
             <div className="navigation">
               <ul className="menu">
                 <li className="nav__item">
-                  <Link to='/home'>Home</Link>
+                  <Link to="/home">Home</Link>
                 </li>
-              
+
                 <li className="nav__item">
-                  <Link to='/shop'>Shop</Link>
+                  <Link to="/shop">Shop</Link>
                 </li>
                 <li className="nav__item">
-                  <Link to='/cart'>Cart</Link>
+                  <Link to="/cart">Cart</Link>
                 </li>
               </ul>
             </div>
             <div className="nav__icons">
-              <span className="fav__icon"><span className='badge'>1</span><i className="bi bi-heart" font-size=""></i></span>
-              <span className="cart__icon"><span className='badge'>1</span><i className="bi bi-cart3" font-size=""></i></span>
-              <span><motion.i whileTap={{ scale:1.2}} className="bi bi-person" font-size=""></motion.i></span>
+              <span className="fav__icon">
+                <span className="badge">1</span>
+                <i className="bi bi-heart" font-size=""></i>
+              </span>
+              <span className="cart__icon">
+                <span className="badge">1</span>
+                <i className="bi bi-cart3" font-size=""></i>
+              </span>
+              <span>
+                <motion.i
+                  whileTap={{ scale: 1.2 }}
+                  className="bi bi-person"
+                  font-size=""
+                ></motion.i>
+              </span>
             </div>
             <div className="mobile__menu">
-              <span><i className="bi bi-list"></i></span>
+              <span>
+                <i className="bi bi-list"></i>
+              </span>
             </div>
           </div>
         </Row>
       </Container>
     </header>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row , Col} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+// import heroImg from ".././assets/images/hero.jpg";
+import heroImg from ".././assets/images/hero2.jpg";
+import "./sytles/Home.css";
 
 const Home = () => {
+
+  const year = new Date().getFullYear();
   return (
-    <div>Home</div>
+
+    <div>
+      <section className='hero__section'>
+        <Container>
+         <Row>
+          <Col lg='6' md='6'>
+            <div className="hero__content">
+              <p className="hero__subtitle">Popular Product in {year}</p>
+              <h2>Create more Minimalistic & Modern Interior</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi optio quibusdam atque veritatis, beatae ad tempore tenetur dolor quisquam!</p>
+              <motion.button whileTap={{ scale:1.2 }} className="buy__btn"><Link to='/shop'>SHOP NOW</Link></motion.button>
+            </div>
+          </Col>
+          <Col lg='6' md=''>
+            <div className="hero__img">
+              <img src={heroImg} alt="heroImg" />
+            </div>
+          </Col>
+          </Row> 
+        </Container>
+      </section>
+      Home
+    </div>
   )
 }
 
